162439571aad1b71b0ebfac85b48c0df609221bf
Josh <josh@Joshs-MacBook-Pro.local>
2018-02-16 20:14:35 -0500

    fully functional now.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,69 @@
 $(document).ready(function() {
 
 	//crystals object for random values per crystal
+
+	var total = 0
+	var wins = 0
+	var losses = 0
+	var ranNo = Math.floor(Math.random() * 101) + 20
+
+	$("#ranNo").text(ranNo)
+
+
 	var crystals = {
 	"yellow" : Math.floor(Math.random() * 12) + 1,
 	"purple" : Math.floor(Math.random() * 12) + 1,
 	"topaz" : Math.floor(Math.random() * 12) + 1,
 	"red" : Math.floor(Math.random() * 12) + 1,
 	}
+function checkTotal(foo, bar, int) {
 
-	function checkTotal(total, ranNo) {
-
-		if (total === ranNo) {
-			wins += 1
-			total = 0
-			ranNo = Math.floor(Math.random() * 101) + 20
+		if (foo < bar) {
+			total += int
 			$("#scoreBox").text(total);
-			$("#wins").text("Wins: " + wins)
-			alert("No one likes Scott");
 		}
 
-		else if (total > ranNo) {
-			losses += 1
-			total = 0
-			ranNo = Math.floor(Math.random() * 101) + 20
-			$("#losses").text("Losses: " + losses)
-			$("#scoreBox").text(total);
+		else if (foo === bar) {
+			wins += 1;
+			ranNo = Math.floor(Math.random() * 101) + 20;
+			total = 0;
+			$("#ranNo").text(bar);
+			$("#wins").text("Wins: " + wins);
+			$("#scoreBox").text(foo);
+		}
+
+		else if (foo > bar) {
+			ranNo = Math.floor(Math.random() * 101) + 20;
+			total = 0;
+			losses += 1;
+			$("#ranNo").text(bar);
+			$("#losses").text("Losses: " + losses);
+			$("#scoreBox").text(foo);
+			return foo
 		}
 	}
-
-
-
-	var ranNo = Math.floor(Math.random() * 101) + 20
-
-	$("#ranNo").text(ranNo)
-
-	var total = 0
-	var wins = 0
-	var losses = 0
+	
 
 
 	$("#yellow").on("click", function() {
-		total += crystals.yellow
-		$("#scoreBox").text(total);
-		console.log(checkTotal(total, ranNo))
+		checkTotal(total, ranNo, crystals.yellow)
+		console.log(`total: ${total} \n ranNo: ${ranNo}`)
 	})
 
 	$("#red").on("click", function() {
-		total += crystals.red
-		$("#scoreBox").text(total);
-		console.log(checkTotal(total, ranNo))
+		checkTotal(total, ranNo, crystals.red)
+		console.log(`total: ${total} \n ranNo: ${ranNo}`)
+
 	})
 
 	$("#topaz").on("click", function() {
-		total += crystals.topaz
-		$("#scoreBox").text(total);
-		console.log(checkTotal(total, ranNo))
+		checkTotal(total, ranNo, crystals.topaz)
+		console.log(`total: ${total} \n ranNo: ${ranNo}`)
 	})
 
 	$("#purple").on("click", function() {
-		total += crystals.purple
-		$("#scoreBox").text(total);
-		console.log(checkTotal(total, ranNo))
+		checkTotal(total, ranNo, crystals.purple)
+		console.log(`total: ${total} \n ranNo: ${ranNo}`)
 	})
 
 })
